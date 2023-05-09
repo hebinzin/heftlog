3adbc82a96f280585da2f96c8893d9695759f52e
hebinzin <hdmj@freeshell.de>
2023-05-09 16:05:16 -0300

    1st commit: templating files and initial drafts

---
+++ heftlog.app.js
@@ -0,0 +1,4 @@
+const S = require("Storage");
+let data = Object.assign({
+  altura: Null
+})
---
+++ heftlog.info.js
@@ -0,0 +1,15 @@
+/*
+ * Run this code at the Espruino Web IDE to generate the `nestor.info`
+ * file in the Bangle.js storage
+ */
+require("Storage").write("heftlog.info",{
+  "id":"heftlog",
+  "name":"Heft Log",
+  "type":"app",
+  "src":"heftlog.app.js",
+  "icon":"heftlog.img",
+  "version":"0.1",
+  "tags":"tool,health"
+  "files":"heftlog.info,heftlog.app.js"
+  "data":"heftlog.json"
+});
