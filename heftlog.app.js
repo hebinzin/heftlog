c0597fb26465cb183ce40dfb4bbf332f2b0f7162
hebinzin <hdmj@freeshell.de>
2023-05-10 07:04:16 -0300

    Added an icon file. Commiting just to push to GitHub and share code to other machines

--- heftlog.app.js
+++ heftlog.app.js
@@ -1,4 +1,71 @@
-const S = require("Storage");
+const S = require('Storage');
+
+// Simplify file saving process
+function saveFile(object, key, value, file)
+{
+  object[key] = value;
+  S.writeJSON(file, object);
+}
+
+/*
+ * VARIABLES:
+ */
+
+// Load data from `heftlog.json` if it exists
 let data = Object.assign({
-  altura: Null
-})
+  // Default values
+  height:null,
+  log:null
+}, S.readJSON('heftlog.json', true) || {});
+
+
+// Menu to allow the user to set his own height
+var heightMenu = {
+  '' : {
+    title : 'Change Height'
+  },
+  '< Back' : () => { E.showMenu(mainMenu); },
+  'Height (cm)' : {
+  	value: data.height,
+  	min: 55, // Chandra Bahadur Dangi
+  	max: 272, // Robert Wadlow
+  	step: 1,
+  	onchange: h => {
+  	  saveFile('heftlog.json', h);
+  	  E.showMenu(mainMenu);
+  	}
+  }
+};
+
+
+if (data.height == null) {
+	E.showMenu(heightMenu);
+}
+
+
+// TODO
+function loadGraph() {
+  E.showMenu();
+}
+// TODO
+function logWeight() {
+  E.showMenu();
+}
+
+// Main menu
+var mainMenu = {
+  '' : { 
+    title : 'Heft Log'
+  },
+  '< Back' : () => { E.showMenu(); },
+  'Load Graph' : loadGraph(),
+  'Log weight (kg)' : {
+    value: data.log[0].weight,
+    min: 2.1, // Lucía Zárate
+    max: 635.0, // Jon Browner Minnoch
+    step: 0.1,
+    onchange: w => logWeight('weight', w)
+  },
+  'Change height' : E.showMenu(heightMenu),
+  'Exit': () => { E.showMenu(); }
+};
