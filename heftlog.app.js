eea58164e7e4e380b972455605ed0e02f5d8d2c7
Heber <hdmj@freeshell.de>
2023-05-11 11:57:10 -0300

    More initial development

--- heftlog.app.js
+++ heftlog.app.js
@@ -1,69 +1,79 @@
+Bangle.loadWidgets();
 const S = require('Storage');
 
 // Simplify file saving process
-function saveFile(object, key, value, file)
-{
+function saveFile(object, key, value, file){
   object[key] = value;
   S.writeJSON(file, object);
 }
 
-// Load data from json file if it exists
-let data = Object.assign({
-  // Default values
+// Load data from json file if it exists, or assign default values
+var data = Object.assign({
   height:null,
   log:null
 }, S.readJSON('heftlog.json', true) || {});
 
-console.log(data);
 
-
-// Menu to allow the user to set his own height
+// Menu to allow users to set their own height
 var heightMenu = {
   '' : {
-    title : 'Change Height'
+    back : () => { E.showMenu(mainMenu); },
   },
-  '< Back' : () => { E.showMenu(mainMenu); },
-  'Height (cm)' : {
-  	value: data.height,
-  	min: 55, // Chandra Bahadur Dangi
-  	max: 272, // Robert Wadlow
-  	step: 1,
-  	onchange: h => {
-  	  saveFile('heftlog.json', h);
-  	  E.showMenu(mainMenu);
-  	}
-  }
+  value: data.height ? data.height : 168,
+  min: 55, // Chandra Bahadur Dangi
+  max: 272, // Robert Wadlow
+  step: 1,
+  onchange: (val) => { saveFile(data, 'height', val, 'heftlog.json'); }
 };
-
-
-if (data.height == null) {
-	E.showMenu(heightMenu);
-}
-
-
-// TODO
-function loadGraph() {
-  E.showMenu();
-}
-// TODO
-function logWeight() {
-  E.showMenu();
-}
 
 // Main menu
 var mainMenu = {
   '' : { 
-    title : 'Heft Log'
+    title : 'Heft Log',
+    back : () => { Bangle.showClock(); },
   },
-  '< Back' : () => { E.showMenu(); },
-  'Load Graph' : loadGraph(),
-  'Log weight (kg)' : {
-    value: data.log[0].weight,
+  'Log weight' : {
+    '' : {
+      back : () => { E.showMenu(mainMenu); },
+    },
+    value: data.log[0].weight ? data.log[0].weight : 65.5,
     min: 2.1, // Lucía Zárate
     max: 635.0, // Jon Browner Minnoch
     step: 0.1,
-    onchange: w => logWeight('weight', w)
+    //onchange:  => logWeight('weight', w)
   },
-  'Change height' : E.showMenu(heightMenu),
-  'Exit': () => { E.showMenu(); }
+  'Load Graph' : () => { loadGraph(); },
+  'Show Log' : () => { showLog(); },
+  'Change height' : heightMenu,
+  'Exit': Bangle.showClock()
 };
+
+// TODO
+function logWeight(a, b){
+  console.log(a);
+  console.log(b);
+  Bangle.showClock();
+}
+
+// TODO
+function loadGraph(){
+  Bangle.showClock();
+}
+
+// TODO
+function showLog(){
+  Bangle.showClock();
+}
+
+if (data.height != null) {
+  E.showMenu(mainMenu);
+} else {
+  E.showMenu({
+    '' : { 
+      title : 'Set your height in cm:',
+      back : () => { Bangle.showClock(); },
+    },
+    'Change height' : heightMenu,
+  });
+  console.log(data.height);
+}
