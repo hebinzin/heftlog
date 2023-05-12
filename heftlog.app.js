89ac901cbb5611b5e8d497fc4490247c65c13531
Heber <hdmj@freeshell.de>
2023-05-12 14:38:42 -0300

    Added a function to list the log
    Added a function to list the log.
    Also, the `prompt_messages.txt` is storing prompt messages to use when logging confirmation gets implemented.

--- heftlog.app.js
+++ heftlog.app.js
@@ -11,12 +11,16 @@ var Profile = Object.assign({
 var HeightMenu = {
   '' : {
     back : () => { E.showMenu(MainMenu); },
-  },  
+  },
   value: Profile.height ? Profile.height : 168,
   min: 55, // Chandra Bahadur Dangi
   max: 272, // Robert Wadlow
   step: 1,
-  onchange: (h) => { saveFile(Profile, 'height', h, FILE); }
+  onchange: h => { 
+    //saveFile(Profile, 'height', h, FILE);
+    Profile.height = h;
+    S.writeJSON(FILE, Profile);
+  }
 };
 
 // Main menu
@@ -33,50 +37,43 @@ var MainMenu = {
     min: 2.1, // Lucía Zárate
     max: 635.0, // Jon Browner Minnoch
     step: 0.1,
-    onchange: (weight) => { logWeight(weight, Profile, FILE); }
+    onchange: weight => { logWeight(weight, Profile, FILE); }
   },
   'Load Graph' : () => { loadGraph(); },
-  'Show Log' : () => { showLog(); },
+  'Show Log' : () => { showLog(Profile.log); },
   'Change height' : HeightMenu,
   'Exit': Bangle.showClock()
 };
 
+// Enter 'W'eight and current date in the 't'ally and save it to 'F'ile
+function logWeight(W, t, F){
+  let d = Date().getDate(),
+  m = Date().getMonth() + 1,
+  y = Date().getFullYear();
 
-// Simplify file saving process
-function saveFile(object, key, value, file){
-  object[key] = value;
-  S.writeJSON(file, object);
-}
-
-// TODO
-function logWeight(w, record, f){
   function _addzero_(x){
     return (x < 10 ? '0' + x.toString() : x.toString());
   }
 
-  let d = Date().getDate(),
-      m = Date().getMonth() + 1,
-      y = Date().getFullYear();
-
   let entry = {
-  	date : y.toString() + _addzero_(m) + _addzero_(d),
-  	heft : w
+  	date : Number(y.toString() + _addzero_(m) + _addzero_(d)),
+  	heft : W
   };
 
-  let prompt;
-  let index = record.log.findIndex(li => li.date === entry.date);
+  if (t.log == null || t.log.length < 1){
+    t.log = [entry];
+  } else {
+    let index = t.log.findIndex(r => r.date === entry.date);
+    if (index >=0 ){
+      t.log[index].heft = entry.heft;
+    } else if (entry.date > t.log[0].date){
+      t.log.unshift(entry);
+    } else return false;
+  }
 
-  if (index >=0 ){
-    record.log[index].heft = entry.heft;
-    prompt = 'Overwrite weight record?';
-  } else if (entry.date > record.log[0].date){
-    record.log.unshift(entry);
-    prompt = 'File a new weight record?';
-  } else return;
-
-  if (E.showPrompt(prompt)){
-    S.writeJSON(f, record);
-  } else return;
+  //TODO: Prompt the user to confirm in case of overwritting
+  S.writeJSON(F, t);
+  return true;
 }
 
 // TODO
@@ -85,8 +82,28 @@ function loadGraph(){
 }
 
 // TODO
-function showLog(){
-  Bangle.showClock();
+function showLog(L){
+
+  function _formatDt_(dt){
+    dt = dt.toString();
+    return dt.slice(2,4) + "-" + dt.slice(4,6) + "-" + dt.slice(6,8);
+  }
+
+  let s = {
+    h: 22,
+    c: L.length,
+    draw: (i, r) => {
+      g.clearRect(r.x,r.y,r.x+r.w-1,r.y+r.h-1);
+      g.setFont('4x6', 3);
+      g.drawString(
+        _formatDt_(L[i].date) + '  ' + L[i].heft
+        , r.x
+        , r.y+6
+      );
+    },
+    back: () => { E.showScroller(); E.showMenu(MainMenu); }
+  };
+  E.showScroller(s);
 }
 
 ///////////////////////////// CODE START //////////////////////////////
