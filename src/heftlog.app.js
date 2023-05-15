1e30fb2439bd08363e172066156f740e30d5c841
hebinzin <hdmj@freeshell.de>
2023-05-15 19:12:24 -0300

    1.0 Release
    Usable app, allowing weight records, setting and changing height, a scrollable log and a line chart. Readme file has some useful information and a license was chosen.

--- src/heftlog.app.js
+++ src/heftlog.app.js
@@ -1,16 +1,15 @@
 const FILE = 'heftlog.json';
 const S = require('Storage');
 
-// Load profile if it exists or assign default values
+// Load the profile if it exists or assign default values
 var Profile = Object.assign({
     height:null,
     log:null
   }, S.readJSON(FILE, true) || {});
 
-// Menu to allow users to set their own height
 var HeightMenu = {
   '' : {
-    back : () => { E.showMenu(MainMenu); },
+    back : () => start(),
   },
   value: Profile.height ? Profile.height : 168,
   min: 55, // Chandra Bahadur Dangi
@@ -22,7 +21,6 @@ var HeightMenu = {
   }
 };
 
-// Main menu
 var MainMenu = {
   '' : {
     title : 'Heft Log',
@@ -36,55 +34,59 @@ var MainMenu = {
     min: 2.1, // Lucía Zárate
     max: 635.0, // Jon Browner Minnoch
     step: 0.1,
-    onchange: weight => { logWeight(weight, Profile, FILE); }
+    onchange: weight => logWeight(weight, Profile)
   },
-  'Show Log' : () => { showLog(Profile.log); },
-  'Load Graph' : () => { loadGraph(Profile.log, Profile.height); },
+  'Show log' : () => showLog(Profile.log),
+  'Load graph' : () => loadGraph(Profile.log, Profile.height),
   'Change height' : HeightMenu,
-  'Exit': Bangle.showClock()
+  'Exit': () => Bangle.showClock()
 };
 
-// Enter current date and 'W'eight in the 't'ally and save it to 'F'ile
-function logWeight(W, t, F){
+// Creates and saves a log entry with Weight and date
+function logWeight(W, profile){
   let d = Date().getDate(),
   m = Date().getMonth() + 1,
   y = Date().getFullYear();
 
-  function _addzero_(x){
+  function _addzero(x){
     return (x < 10 ? '0' + x.toString() : x.toString());
   }
 
+  // An entry has a 'YYYYMMDD' date and the seconds since 1970
   let entry = {
-    date : Number(y.toString() + _addzero_(m) + _addzero_(d)),
+    date : Number(y.toString() + _addzero(m) + _addzero(d)),
     timestamp : Math.round(Date.now() / 1000),
   	heft : W
   };
 
-  if (t.log == null || t.log.length < 1){
-    t.log = [entry];
+  if (profile.log == null || profile.log.length < 1){
+    // If there's no log yet, create one with the current entry
+    profile.log = [entry];
   } else {
-    let index = t.log.findIndex(r => r.date === entry.date);
+    let index = profile.log.findIndex(r => r.date === entry.date);
     if (index >=0 ){
-      t.log[index].heft = entry.heft;
-    } else if (entry.date > t.log[0].date){
-      t.log.unshift(entry);
+      // Overwrite an existing entry in the same day
+      profile.log[index].heft = entry.heft;
+    } else if (entry.date > profile.log[0].date){
+      // A new day entry is placed in the top of the log
+      profile.log.unshift(entry);
     } else return false;
   }
 
   //TODO: Prompt the user to confirm in case of overwritting
-  S.writeJSON(F, t);
+  S.writeJSON(FILE, t);
   return true;
 }
 
+// Turns the date into a string for displaying
 function formatDt(dt, separator){
   dt = dt.toString();
   return dt.slice(2,4) + separator + dt.slice(4,6) + separator + dt.slice(6,8);
 }
 
-// Takes the heft log and show it as a scrollable list
+// Takes the Log and displays it as a scrollable list
 function showLog(L){
   E.showMenu();
-
   let list = {
     h: 30,
     c: L.length,
@@ -93,39 +95,38 @@ function showLog(L){
        .setFontAlign(-1).drawString(formatDt(L[i].date, '-'), r.x+6, r.y+10)
        .setFontAlign(1).drawString(L[i].heft.toFixed(1), r.w-6, r.y+10);
     },
-    back: () => { E.showScroller().showMenu(MainMenu); },
-    select : i => { return;}
+    back: () => start(),
+    select : i => { return; }
   };
   E.showScroller(list);
 }
 
+// A graphic view for the Log
 function loadGraph(L, height){
   E.showMenu();
 
-  // The app usage timespan (interval from first to last timestamp)
+  // Calculate the interval between the first and the last entry
   let span = L[0].ts - L[L.length-1].ts;
-  // To store the topmost and b
+  
+  // Placeholders for the minimum and maximum weight values
   let bottom = L[0].heft, top = bottom;
 
   for (i = 0; i < L.length; i++){
-    // Each entry also gets its span calculated
-    // which is then divided by the total span.
-    // This gives us the coordinate as a percentage value
-    L[i].x = ((L[i].ts - L[L.length-1].ts) / span).toFixed(3);
-
-    // Check wether the current heft is the highest or lowest
+    // Check if the current weight is the highest or the lowest
     if (L[i].heft > top){
       top = L[i].heft;
     } else if (L[i].heft < bottom){
       bottom = L[i].heft;
     }
+    // Calculate and divide each entry's "span" by the total span
+    // The result is the horizontal coordinate as a screen percentage
+    L[i].x = ((L[i].ts - L[L.length-1].ts) / span).toFixed(3);
   }
 
-  // We'll use the same iteration to get the lowest and highest heft
-  let variance = top - bottom; // How much the user's weight changed
+  let variance = top - bottom; // How much the user weight has changed
 
-  // Calculate the 'y' coordinates (weight) for each log entry:
   for (j = 0; j < L.length; j++){
+    // Calculate the vertical coordinate as a screen percentage
     L[j].y = (1 -((L[j].heft - bottom) / variance)).toFixed(3);
   }
 
@@ -135,13 +136,15 @@ function loadGraph(L, height){
   const offset = g.getHeight() - R.h; // The space used by the widgets
 
   for (k = L.length - 2; k >= 0; k--){
-
-    let imc = L[k].heft / Math.pow(height / 100, 2);
-    if (imc > 30 || imc < 17) g.setColor(1, 0, 0);
-    else if (imc > 25 || imc < 18.5) g.setColor(1, 1, 0);
-    else if (imc > 21 && imc < 23) g.setColor(0, 1, 0);
+    // Color coding the the graph line with user's BMI range
+    let bmi = L[k].heft / Math.pow(height / 100, 2);
+    if (bmi > 30 || bmi < 17) g.setColor(1, 0, 0);
+    else if (bmi > 25 || bmi < 18.5) g.setColor(1, 1, 0);
+    else if (bmi > 21 && bmi < 23) g.setColor(0, 1, 0);
     else g.setColor(1, 1, 1);
 
+    // The line's vertices derive from multiplying the percentage
+    // coordinates by the graph's dimensions (and adding the gaps)
     g.drawLine(
       L[k+1].x * R.w,
       L[k+1].y * graphHeight + offset + gap,
@@ -152,7 +155,7 @@ function loadGraph(L, height){
     g.reset();
   }
 
-  g // Writes top and bottom weight and last and first entry date:
+  g // Displays weight and date edge values on the graph
    .setFont('4x6', 2).setFontAlign(0, 0)
    .drawString(top, R.w / 2, gap / 2 + offset)
    .drawString(bottom, R.w / 2, R.h - (gap / 2) + offset)
@@ -160,33 +163,32 @@ function loadGraph(L, height){
    .drawString(formatDt(L[0].date, '/'), 4, R.h / 2  + offset)
    .drawString(formatDt(L[L.length-1].date, '/'), R.w - 4, R.h / 2 + offset);
 
-  setWatch(
-    () => { 
-      if (Bangle.isLocked() == false){
-        g.clear();
-        E.showMenu(MainMenu);
-      }
-    },
-    BTN1,
-    {
-      repeat: false,
-      edge: 'falling'
+  // Enables returning with the back widget or button press
+  Bangle.setUI({
+    mode: "custom",
+    back: () => {
+      start();
     }
-  );
+  });
 }
 
-///////////////////////////// CODE START //////////////////////////////
-Bangle.loadWidgets();
-
-if (Profile.height != null) {
+// Streamline starting process
+function start(){
+  g.clear(reset);
+  Bangle.loadWidgets();
+  Bangle.drawWidgets();
   E.showMenu(MainMenu);
+}
+
+// App start prompts for user height if it isn't set
+if (Profile.height != null) {
+  start();
 } else {
   E.showMenu({
     '' : {
-      title : 'Set your height in cm:',
+      title : 'Set height (cm)',
       back : () => { Bangle.showClock(); },
     },
     'Change height' : HeightMenu,
   });
-  console.log(Profile.height);
 }
